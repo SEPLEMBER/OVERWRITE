dd6420f6fd4b59d6f233dff70627a62e382daae5
SEPLEMBER <seoptembe4@gmail.com>
2025-06-15 12:29:11 +0700

    Update LockActivity.java

--- app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
