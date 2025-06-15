1935daa31aac8b7ef35708bbe3544d601420d4ea
SEPLEMBER <seoptembe4@gmail.com>
2025-06-15 17:55:10 +0700

    Update Constants.java

--- app/src/main/java/com/aidinhut/simpletextcrypt/Constants.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/Constants.java
@@ -1,9 +1,8 @@
 package com.aidinhut.simpletextcrypt;
 
 public class Constants {
-    public static final String PREFERENCES_KEY = "com.aidinhut.SimpleTextCrypt.Preferences";
-    public static final String ENCRYPTION_KEY_SETTINGS_KEY = "EncryptionKeySettingsKey";
-    public static final String LOCKSCREEN_PASSWORD_SETTINGS_KEY = "LockscreenPasswordSettingsKey";
-    public static final String LOCK_TIMEOUT_SETTINGS_KEY = "LockTimeoutSettingsKey";
+    public static final String PREFERENCES_KEY = "SimpleTextCryptPrefs";
+    public static final String LOCK_TIMEOUT_SETTINGS_KEY = "lockTimeout";
     public static final String DEFAULT_LOCKSCREEN_PASSWORD = "12345678";
+    public static final String DEFAULT_ENCRYPTION_KEY = "1111111111";
 }
