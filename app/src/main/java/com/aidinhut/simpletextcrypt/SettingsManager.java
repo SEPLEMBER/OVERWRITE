04d5c1f7b7f5a661d70d85ecf7137b7eea2e385a
SEPLEMBER <seoptembe4@gmail.com>
2025-06-15 12:11:18 +0700

    Update SettingsManager.java

--- app/src/main/java/com/aidinhut/simpletextcrypt/SettingsManager.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/SettingsManager.java
@@ -3,146 +3,57 @@ package com.aidinhut.simpletextcrypt;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.aidinhut.simpletextcrypt.exceptions.EncryptionKeyNotSet;
-import com.aidinhut.simpletextcrypt.exceptions.SettingsNotSavedException;
-import com.aidinhut.simpletextcrypt.exceptions.WrongPasscodeException;
-
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-
 public class SettingsManager {
-
     private static SettingsManager instance;
-    private char[] passcode = "1111".toCharArray();
+    private SharedPreferences preferences;
 
-    private SettingsManager() {
+    private SettingsManager(Context context) {
+        preferences = context.getSharedPreferences(Constants.PREFERENCES_KEY, Context.MODE_PRIVATE);
     }
 
-    public static SettingsManager getInstance() {
+    public static SettingsManager getInstance(Context context) {
         if (instance == null) {
-            instance = new SettingsManager();
+            instance = new SettingsManager(context);
         }
         return instance;
     }
 
-    public String tryGetPasscode(String passcode, Context context)
-            throws UnsupportedEncodingException, GeneralSecurityException, WrongPasscodeException {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-
-        if (!sharedPref.contains(Constants.PASSCODE_SETTINGS_KEY)) {
+    public String getPasscode(Context context) {
+        String passcode = preferences.getString(Constants.PASSCODE_SETTINGS_KEY, null);
+        if (passcode == null) {
             return Constants.DEFAULT_PASSCODE;
         }
-
-        this.passcode = passcode.toCharArray();
-
-        try {
-            String result = Crypter.decrypt(this.passcode,
-                    sharedPref.getString(Constants.PASSCODE_SETTINGS_KEY, Constants.DEFAULT_PASSCODE));
-            return result;
-        } catch (IllegalBlockSizeException | BadPaddingException error) {
-            Arrays.fill(this.passcode, '\0'); // Очистка пароля
-            throw new WrongPasscodeException(context);
-        }
+        return passcode;
     }
 
-    public String getPasscode(Context context)
-            throws UnsupportedEncodingException, GeneralSecurityException {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-
-        if (!sharedPref.contains(Constants.PASSCODE_SETTINGS_KEY)) {
-            return Constants.DEFAULT_PASSCODE;
-        }
-
-        return Crypter.decrypt(this.passcode,
-                sharedPref.getString(Constants.PASSCODE_SETTINGS_KEY, Constants.DEFAULT_PASSCODE));
-    }
-
-    public void setPasscode(String passcode, Context context)
-            throws UnsupportedEncodingException, GeneralSecurityException, SettingsNotSavedException {
-        char[] newPasscode = passcode.toCharArray();
-
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor prefEditor = sharedPref.edit();
-
-        prefEditor.putString(Constants.PASSCODE_SETTINGS_KEY,
-                Crypter.encrypt(newPasscode, new String(newPasscode)));
-
-        if (!prefEditor.commit()) {
-            Arrays.fill(newPasscode, '\0');
-            throw new SettingsNotSavedException(context);
-        }
-
-        Arrays.fill(this.passcode, '\0'); // Очистка старого пароля
-        this.passcode = newPasscode;
-    }
-
-    public String getEncryptionKey(Context context)
-            throws UnsupportedEncodingException, GeneralSecurityException, EncryptionKeyNotSet {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-
-        if (!sharedPref.contains(Constants.ENCRYPTION_KEY_SETTINGS_KEY)) {
-            return "";
-        }
-
-        return Crypter.decrypt(this.passcode,
-                sharedPref.getString(Constants.ENCRYPTION_KEY_SETTINGS_KEY, ""));
-    }
-
-    public void setEncryptionKey(String key, Context context)
-            throws UnsupportedEncodingException, GeneralSecurityException, SettingsNotSavedException {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor prefEditor = sharedPref.edit();
-
-        prefEditor.putString(Constants.ENCRYPTION_KEY_SETTINGS_KEY,
-                Crypter.encrypt(this.passcode, key));
-
-        if (!prefEditor.commit()) {
-            throw new SettingsNotSavedException(context);
-        }
-    }
-
-    public void setLockTimeout(String timeout, Context context) throws SettingsNotSavedException {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor prefEditor = sharedPref.edit();
-
-        int intTimeout = 0;
-        if (timeout != null && !timeout.isEmpty()) {
-            intTimeout = Integer.parseInt(timeout);
-        }
-
-        prefEditor.putInt(Constants.LOCK_TIMEOUT_SETTINGS_KEY, intTimeout);
-
-        if (!prefEditor.commit()) {
-            throw new SettingsNotSavedException(context);
-        }
+    public void setPasscode(String passcode, Context context) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(Constants.PASSCODE_SETTINGS_KEY, passcode);
+        editor.apply();
     }
 
     public int getLockTimeout(Context context) {
-        SharedPreferences sharedPref = context.getSharedPreferences(Constants.PREFERENCES_KEY,
-                Context.MODE_PRIVATE);
-
-        if (!sharedPref.contains(Constants.LOCK_TIMEOUT_SETTINGS_KEY)) {
-            return 0;
-        }
-
-        return sharedPref.getInt(Constants.LOCK_TIMEOUT_SETTINGS_KEY, 0);
+        return preferences.getInt(Constants.LOCK_TIMEOUT_SETTINGS_KEY, 5);
     }
 
-    // Очистка passcode при уничтожении объекта
-    public void clearPasscode() {
-        if (this.passcode != null) {
-            Arrays.fill(this.passcode, '\0');
-            this.passcode = null;
+    public void setLockTimeout(String timeout, Context context) {
+        try {
+            int timeoutInt = Integer.parseInt(timeout);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt(Constants.LOCK_TIMEOUT_SETTINGS_KEY, timeoutInt);
+            editor.apply();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid lock timeout value");
         }
+    }
+
+    public String getEncryptionKey(Context context) {
+        return preferences.getString(Constants.ENCRYPTION_KEY_SETTINGS_KEY, "");
+    }
+
+    public void setEncryptionKey(String key, Context context) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(Constants.ENCRYPTION_KEY_SETTINGS_KEY, key);
+        editor.apply();
     }
 }
