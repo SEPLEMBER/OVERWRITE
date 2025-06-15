4ea1c14421551a4d80fed1829ffe9d92c225a72c
SEPLEMBER <seoptembe4@gmail.com>
2025-06-15 17:44:43 +0700

    Update LockActivity.java

--- app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
@@ -2,11 +2,8 @@ package com.aidinhut.simpletextcrypt;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LockActivity extends AppCompatActivity {
@@ -15,42 +12,23 @@ public class LockActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
-
-        ((EditText)findViewById(R.id.passcodeEditText)).setOnEditorActionListener(
-                new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                        if (actionId == EditorInfo.IME_ACTION_DONE) {
-                            unlock();
-                            return true;
-                        }
-                        return false;
-                    }
-                }
-        );
     }
 
     public void onUnlockButtonClicked(View view) {
-        unlock();
-    }
-
-    private void unlock() {
-        EditText passcodeBox = findViewById(R.id.passcodeEditText);
-        String enteredPassword = passcodeBox.getText().toString();
-        passcodeBox.setText("");
+        EditText passcodeEditText = findViewById(R.id.passcodeEditText);
+        String passcode = passcodeEditText.getText().toString();
 
         try {
-            if (!SettingsManager.getInstance(this).verifyLockscreenPassword(enteredPassword)) {
+            if (SettingsManager.getInstance(this).verifyLockscreenPassword(passcode)) {
+                // Проверяем ключ шифрования (он должен быть установлен по умолчанию)
+                SettingsManager.getInstance(this).getDecryptedEncryptionKey(passcode, this);
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            } else {
                 Utilities.showErrorMessage(getString(R.string.wrong_lockscreen_password_error), this);
-                return;
             }
-            String decryptedEncryptionKey = SettingsManager.getInstance(this).getDecryptedEncryptionKey(enteredPassword, this);
-            Intent newIntent = new Intent(this, MainActivity.class);
-            newIntent.putExtra("encryption_key", decryptedEncryptionKey);
-            startActivity(newIntent);
-            finish();
-        } catch (Exception e) {
-            Utilities.showErrorMessage(e.getMessage(), this);
+        } catch (Exception error) {
+            Utilities.showErrorMessage(error.getMessage(), this);
         }
     }
 }
