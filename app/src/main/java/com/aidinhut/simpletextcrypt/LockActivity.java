74c46d8b683788625a19e7cd8824672ab8bdd4f8
SEPLEMBER <seoptembe4@gmail.com>
2025-06-15 12:09:25 +0700

    Update LockActivity.java

--- app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/LockActivity.java
@@ -1,35 +1,9 @@
-/*
- * This file is part of SimpleTextCrypt.
- * Copyright (c) 2015-2020, Aidin Gharibnavaz <aidin@aidinhut.com>
- *
- * SimpleTextCrypt is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SimpleTextCrypt is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SimpleTextCrypt.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.aidinhut.simpletextcrypt;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,7 +15,6 @@ public class LockActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
 
-        // Handling pressing Enter key on the keyboard. It should automatically unlock the app.
         ((EditText)findViewById(R.id.passcodeEditText)).setOnEditorActionListener(
                 new TextView.OnEditorActionListener() {
                     @Override
@@ -63,25 +36,15 @@ public class LockActivity extends AppCompatActivity {
     private void unlock() {
         EditText passcodeBox = (EditText)findViewById(R.id.passcodeEditText);
         String passcode = passcodeBox.getText().toString();
-
-        // Clearing passcode text box.
         passcodeBox.setText("");
 
-        String savedPasscode;
-        try {
-            savedPasscode = SettingsManager.getInstance().tryGetPasscode(passcode, this);
-        } catch (Exception error) {
-            // Any other errors.
-            Utilities.showErrorMessage(error.getMessage(), this);
-            return;
-        }
+        String savedPasscode = SettingsManager.getInstance(this).getPasscode(this);
 
         if (savedPasscode.compareTo(passcode) != 0) {
             Utilities.showErrorMessage(getString(R.string.wrong_passcode_error), this);
             return;
         }
 
-        // Right password. Continue to the other activity.
         Intent newIntent = new Intent(this, MainActivity.class);
         startActivity(newIntent);
     }
