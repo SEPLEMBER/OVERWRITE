37086d1f40f82eae8d4aa58cf4c13e0626967fd6
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-06-27 11:53:09 +0700

    Update SettingsActivity.java
    commit: Add FLAG_SECURE to SettingsActivity to prevent screenshots
    - Added WindowManager.LayoutParams.FLAG_SECURE in onCreate to disable screenshots and screen recording.

--- app/src/main/java/com/aidinhut/simpletextcrypt/SettingsActivity.java
+++ app/src/main/java/com/aidinhut/simpletextcrypt/SettingsActivity.java
@@ -26,13 +26,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.WindowManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, 
+                            WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_settings);
 
         loadPreviousSettings();
